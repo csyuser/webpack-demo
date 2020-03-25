78c05133cebe14175942965802ab53fdf982de1d
csy <13540039093@163.com>
2020-03-25 16:58:14 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+      title: 'test',
+      template: 'src/assets/index.html'
+    }
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,  //正则表达式
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
